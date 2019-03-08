5964ccf03970031f577e6c28a13fe525929f5e1d
John Unwin <junwin@outlook.com>
2019-03-07 21:23:36 -0600

    Minor refactor, change paths  for docker

--- HousePriceService/Controllers/HousePriceController.cs
+++ HousePriceService/Controllers/HousePriceController.cs
@@ -1,13 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.ML;
-using Microsoft.ML.Core.Data;
-using System;
-using System.IO;
 
 namespace HousePriceService.Controllers
 {
@@ -19,38 +11,10 @@ namespace HousePriceService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            var price = test();
-            return new string[] { price };
-        }
-
-        private string test()
-        {
-            // Run a few test examples
             var housePriceSample1 = new HouseData() { Area = "76", BedRooms = 3, BedRoomsBsmt = 0, FullBath = 2, HalfBath = 0, Rooms = 7, ApproxSquFeet = 1300, GarageType = "Attached", GarageSpaces = 2 };
-            return PredictSinglePrice(housePriceSample1);
+            var price = HousePricePrediction.PredictSinglePrice(housePriceSample1, @"MLNETModels\housePriceModel.zip");
 
-        }
-
-        public static string PredictSinglePrice(HouseData houseData, string outputModelPath = @"C:\Users\junwi\Source\Repos\HousePriceService\HousePriceService\bin\Debug\netcoreapp2.2\MLNETModels\housePriceModel.zip")
-        {
-            //  Load the prediction model we saved earlier
-            MLContext mlContext = new MLContext(seed: 0);
-            ITransformer loadedModel;
-            using (var stream = new FileStream(outputModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                loadedModel = mlContext.Model.Load(stream);
-            }
-
-            // Creete a handy function based on our HouseData class and a class to contain the result
-            var predictionFunction = loadedModel.CreatePredictionEngine<HouseData, HousePrediction>(mlContext);
-
-            // Predict the Sale price - TA DA
-            var prediction = predictionFunction.Predict(houseData);
-
-            var pv = prediction.SoldPrice;
-            return string.Format("Predicted Price is {0}", pv);
-
-            
+            return new string[] { price };
         }
     }
 }
