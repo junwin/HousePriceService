687a8f639ed3a81286f84db2e3a06c4e93de19b1
John Unwin <jdunwin@gmail.com>
2019-03-15 22:06:08 -0500

    Change to method invocation

--- HousePriceService/Controllers/HousePriceController.cs
+++ HousePriceService/Controllers/HousePriceController.cs
@@ -20,13 +20,12 @@ namespace HousePriceService.Controllers
 
         // GET api/HousePrice/price
         [HttpGet("{price}")]
-        public ActionResult<string> Get(string area, float sqft, float rooms, float bedrooms, float fullbath, float halfbath, string garageType, float garageSpaces)
+        public ActionResult<IEnumerable<string>> Get(string area, float approxSquFeet, float rooms, float bedrooms, float fullbath, float halfbath, string garageType, float garageSpaces)
         {
-            var housePriceSample1 = new HouseData() { Area = area, BedRooms = bedrooms, BedRoomsBsmt = 0, FullBath = fullbath, HalfBath =halfbath, Rooms = rooms, ApproxSquFeet = sqft, GarageType = garageType, GarageSpaces = garageSpaces };
-            //var price = HousePricePrediction.PredictSinglePrice(housePriceSample1, @"MLNETModels\housePriceModel.zip");
+            var housePriceSample1 = new HouseData() { Area = area, BedRooms = bedrooms, BedRoomsBsmt = 0, FullBath = fullbath, HalfBath =halfbath, Rooms = rooms, ApproxSquFeet = approxSquFeet, GarageType = garageType, GarageSpaces = garageSpaces };
             var price = HousePricePrediction.PredictSinglePrice(housePriceSample1);
-
-            return price.ToString();
+            return new string[] { price };
+            
             
         }
     }
